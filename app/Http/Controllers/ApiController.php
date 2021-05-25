4a071780d56f6d1536b0c08f1200ca038adf2331
Sanjeev Kamboj <sanjeev_kamboj@42works.net>
2021-05-26 01:28:13 +0530

    admin setup and api's login,register,setpassword , reset password

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace App\Http\Controllers;
+use JWTAuth;
+use Validator;
+use IlluminateHttpRequest;
+use AppHttpRequestsRegisterAuthRequest;
+use TymonJWTAuthExceptionsJWTException;
+use SymfonyComponentHttpFoundationResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\User;
+use App\Models\Admin;
+use Mail, Hash, Auth;
+class ApiController extends Controller
+{
+    public function user_registration(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'first_name'    => 'required',
+                'last_name'     => 'required',
+                'email'         => 'required|email',
+                'password'      => 'required',
+                'mobile_number' => 'required|numeric'
+            ]
+        );
+
+        if ($validator->fails()) {
+
+            return response()->json(['error' => $validator->errors()], 200);
+        }
+
+        $check_email_exists = User::where('email', $data['email'])->first();
+        if (!empty($check_email_exists)) {
+            return response()->json(['error' => 'This Email is already exists.'], 200);
+        }
+
+
+        $user                       = new User();
+        $user->first_name           = $data['first_name'];
+        $user->last_name            = $data['last_name'];
+        $user->email                = $data['email'];
+        $user->mobile_number        = $data['mobile_number'];
+        $hash_password              = Hash::make($data['password']);
+        $user->password             = str_replace("$2y$", "$2a$", $hash_password);
+        $user->status               = 'Active';
+        if ($user->save()) {
+            return response()->json(['success' => true, 'data' => $user], Response::HTTP_OK);
+        } else {
+            return response()->json(['error' => false, 'data' => 'Something went wrong, Please try again later.']);
+        }
+    }
+
+    public function user_login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        // print_r('here'); die;
+
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'email'      => 'required|email',
+                'password'   => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 200);
+        }
+        $token = auth()->attempt($credentials);
+        if ($token ) {
+            return $this->respondWithToken($token);
+        } else {
+            $response = ["message" => 'Invalid Details'];
+            return response($response, 422);
+        }
+    }
+
+    public function forgot_password(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'email'      => 'required|email',
+            ]
+        );
+
+        if ($validator->fails()) {
+
+            return response()->json(['error' => $validator->errors()], 200);
+        }
+
+
+        $check_email_exists = User::where('email', $request['email'])->first();
+        if (empty($check_email_exists)) {
+            return response()->json(['error' => 'Email not exists.'], 200);
+        }
+
+
+        $check_email_exists->secret_key           =  rand(1111, 9999);
+        if ($check_email_exists->save()) {
+            $project_name = env('App_name');
+            $email = $request['email'];
+            try {
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+                    Mail::send('emails.user_forgot_password_api', ['name' => ucfirst($check_email_exists['first_name']) . ' ' . $check_email_exists['last_name'], 'otp' => $check_email_exists['secret_key']], function ($message) use ($email, $project_name) {
+                        $message->to($email, $project_name)->subject('User Forgot Password');
+                    });
+                }
+            } catch (Exception $e) {
+            }
+            return response()->json(['success' => true, 'data' => 'Email sent on registered Email-id.'], Response::HTTP_OK);
+        } else {
+            return response()->json(['error' => false, 'data' => 'Something went wrong, Please try again later.']);
+        }
+    }
+
+    public function reset_password(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'secret_key'       =>  'required|numeric',
+                'email'      => 'required|email',
+                'password'   => 'required',
+                'confirm_password' => 'required_with:password|same:password'
+            ]
+        );
+
+        if ($validator->fails()) {
+
+            return response()->json(['error' => $validator->errors()], 200);
+        }
+
+
+        $email = $data['email'];
+        $check_email = User::where('email', $email)->first();
+        if (empty($check_email['secret_key'])) {
+            return response()->json(['error' => 'Something went wrong, Please try again later.']);
+        }
+        if (empty($check_email)) {
+            return response()->json(['error' => 'This Email-id is not exists.']);
+        } else {
+            if ($check_email['secret_key'] == $data['secret_key']) {
+                $hash_password                  = Hash::make($data['password']);
+                $check_email->password          = str_replace("$2y$", "$2a$", $hash_password);
+                $check_email->secret_key               = null;
+                if ($check_email->save()) {
+                    return response()->json(['success' => true, 'message' => 'Password changed successfully.']);
+                } else {
+                    return response()->json(['error' => 'Something went wrong, Please try again later.']);
+                }
+            } else {
+                return response()->json(['error' => 'secret_key mismatch']);
+            }
+        }
+    }
+
+    public function profile(Request $request)
+    {
+
+        try {
+            $user = auth()->userOrFail();
+        } catch (\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e) {
+            return response()->json(['error' => $e->getMessage()], 200);
+        }
+
+        return response()->json(['success' => true, 'data' => $user], 200);
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $data = $request->all();
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'first_name' => 'required',
+                'mobile_number' => 'required|numeric'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 200);
+        }
+
+        $user_id = Auth::User()->id;
+        $update_profile =  User::where('id',$user_id)->first();
+        $update_profile->first_name         = $data['first_name'];
+        $update_profile->mobile_number      = $data['mobile_number'];
+        if ($update_profile->save()) {
+            return response()->json(['success' => true, 'data' => 'User Profile Updated Successfully.'], Response::HTTP_OK);
+        }else{
+             return response()->json(['error' => 'Something went wrong, Please try again later.']);
+        }
+
+    }
+
+ 
+    public function logout(Request $request)
+    {
+        print_r($request);die;
+        try {
+            JWTAuth::invalidate($request->token);
+
+            return response()->json([
+                'success' => true,
+                'message' => 'User logged out successfully'
+            ]);
+        } catch (JWTException $exception) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Sorry, the user cannot be logged out'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+
+
+    public function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'code' => 200,
+            'expire_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }}
---
+++ resources/views/backEnd/layouts/master.blade.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<!-- BEGIN HEAD -->
+	<head>
+		<meta charset="utf-8"/>
+		<title>{{ PROJECT_NAME }} - @yield('title')</title>
+		<link rel="shortcut icon" href="favicon.ico"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta content="width=device-width, initial-scale=1" name="viewport"/>
+		<meta content="" name="description"/>
+		<meta content="" name="author"/>
+		<!-- BEGIN GLOBAL MANDATORY STYLES -->
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css"/>
+		<link href="{{ url(backEndCssPath.'/font-awesome.css') }}" rel="stylesheet" type="text/css"/>
+		<link href="{{ url(backEndCssPath.'/simple-line-icons.min.css') }}" rel="stylesheet" type="text/css"/>
+		<link href="{{ url(backEndCssPath.'/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
+		<link href="{{ url(backEndCssPath.'/uniform.default.css') }}" rel="stylesheet" type="text/css"/>
+		<link href="{{ url(backEndCssPath.'/developer.css') }}" rel="stylesheet" type="text/css"/>
+		<!-- END PAGE LEVEL PLUGIN STYLES -->
+		<!-- BEGIN PAGE STYLES -->
+		<link href="{{ url(backEndCssPath.'/tasks.css') }}" rel="stylesheet" type="text/css"/>
+		<!-- END PAGE STYLES -->
+		<!-- BEGIN THEME STYLES -->
+		<link href="{{ url(backEndCssPath.'/components.css') }}" rel="stylesheet" type="text/css"/>
+		<link href="{{ url(backEndCssPath.'/plugins.css') }}" rel="stylesheet" type="text/css"/>
+		<link href="{{ url(backEndCssPath.'/layout.css') }}" rel="stylesheet" type="text/css"/>
+		<link href="{{ url(backEndCssPath.'/darkblue.css') }}" rel="stylesheet" type="text/css" id="style_color"/>
+		<link href="{{ url(backEndCssPath.'/custom.css') }}" rel="stylesheet" type="text/css"/>
+		<link href="{{ url(backEndCssPath.'/margins-min.css') }}" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="{{ url('public/css/croppie.css') }}">
+		<!-- END THEME STYLES -->
+		<!-- <link href="{{ url('public/backEnd/css/datepicker.css') }}" rel="stylesheet" type="text/css"/> -->
+		<!-- <link href="{{ url('public/backEnd/css/datepicker3.css') }}" rel="stylesheet" type="text/css"/>  -->
+		<!-- <link rel="stylesheet" type="text/css" href="../../assets/global/plugins/bootstrap-datepicker/css/datepicker3.css"/> -->
+		<!-- <link rel="stylesheet" type="text/css" href="../../assets/global/plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css"/> -->
+		<!-- <link rel="stylesheet" type="text/css" href="../../assets/global/plugins/bootstrap-colorpicker/css/colorpicker.css"/> -->
+		<!-- <link rel="stylesheet" type="text/css" href="{{ url('public/backEnd/css/daterangepicker-bs3.css') }}"/>
+		<link rel="stylesheet" type="text/css" href="{{ url('public/backEnd/css/bootstrap-datetimepicker.min.css') }}"/> -->
+
+		<!-- data-table start -->
+		<link rel="stylesheet" type="text/css" href="{{ url(backEndCssPath.'/select2.css') }}"/>
+		<link rel="stylesheet" type="text/css" href="{{ url(backEndCssPath.'/dataTables.scroller.min.css') }}"/>
+		<link rel="stylesheet" type="text/css" href="{{ url(backEndCssPath.'/dataTables.colReorder.min.css') }}"/>
+		<link rel="stylesheet" type="text/css" href="{{ url(backEndCssPath.'/dataTables.bootstrap.css') }}"/>
+		<!-- data-table end -->
+<script src="{{ url(backEndJsPath.'/full_screen.js') }}"></script>
+		<script src="{{ url(backEndJsPath.'/jquery.min.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/jquery-migrate.min.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/jquery.toaster.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/jquery.validate.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/additional-methods.min.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/common.js') }}" type="text/javascript"></script>
+
+		<script src="{{ url('public/backEnd/js/bootstrap-datepicker.js') }}" type="text/javascript"></script>
+ 		<script src="{{ url('public/backEnd/js/range-datetimepicker.js') }}" type="text/javascript"></script>
+ 		<script src="{{ url('public/backEnd/js/jquery.datetimepicker.js') }}" type="text/javascript"></script>
+ 		<script src="{{ url('public/backEnd/js/jquery.timepicker.min.js') }}" type="text/javascript"></script>
+ 		<script src="{{url('public/backEnd/js/datepair.js')}}" type="text/javascript" ></script>
+		<script src="{{url('public/backEnd/js/jquery.datepair.js')}}" type="text/javascript" ></script>
+		<script src="{{ url('public/js/demo.js') }}" type="text/javascript"></script>
+		<script src="{{ url('public/js/croppie.js') }}" type="text/javascript"></script>
+		<script src="{{ url('public/js/croppie-demo.js') }}" type="text/javascript"></script>
+		<style type="text/css">
+			.page-header.navbar {
+				height: 65px;
+				min-height: 65px;
+			}
+			.page-header.navbar .page-logo {
+				height: 65px;
+			}
+			#header_lgo {
+				height: auto;
+				width: 115px;
+				margin: 5px 0;
+				object-fit: cover;
+			}
+			.page-header.navbar .top-menu {
+				height: 65px;
+			}
+			.page-header.navbar .top-menu .navbar-nav {
+				height: 100%;
+			}
+			.page-header.navbar .top-menu .navbar-nav > li.dropdown {
+				height: 65px;
+			}
+			.page-header.navbar .top-menu .navbar-nav > li.dropdown-user .dropdown-toggle {
+				padding: 20px 12px 0 0;
+			}
+			.page-header.navbar .top-menu .navbar-nav > li.dropdown-user .dropdown-toggle > img {
+				height: 35px;
+				width: 35px;
+				object-fit: cover;
+			}
+			.page-header-fixed .page-container {
+				margin-top: 65px;
+			}
+		</style>
+	</head>
+	<!-- END HEAD -->
+
+	<!-- BEGIN BODY -->
+	<body class="page-header-fixed page-quick-sidebar-over-content">
+		@include('backEnd.common.header')
+
+		<!-- BEGIN CONTAINER -->
+		<div class="page-container">
+			@include('backEnd.common.notification')
+			
+			@include('backEnd.common.sidebar')
+			@yield('content')
+		</div>
+
+		<!-- BEGIN FOOTER -->
+		<div class="page-footer">
+			<div class="page-footer-inner">
+				Copyright © Laravel 8 <?php echo date('Y'); ?>. All Rights Reserved.
+			</div>
+			<div class="scroll-to-top">
+				<i class="fa fa-arrow-circle-up"></i>
+			</div>
+		</div>
+		<!-- END FOOTER -->
+		
+		<!--Load jquery-ui-1.10.3.custom.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
+		<script src="{{ url(backEndJsPath.'/validation/common_validation.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/jquery-ui-1.10.3.custom.min.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/bootstrap.min.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/bootstrap-hover-dropdown.min.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/jquery.slimscroll.min.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/jquery.blockui.min.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/jquery.cokie.min.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/jquery.uniform.min.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/bootstrap-switch.min.js') }}" type="text/javascript"></script>
+		<!-- END CORE PLUGINS -->
+		
+		<!-- BEGIN PAGE LEVEL SCRIPTS -->
+		<script src="{{ url(backEndJsPath.'/metronic.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/layout.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/quick-sidebar.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/demo.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/index.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/tasks.js') }}" type="text/javascript"></script>
+		<!-- END PAGE LEVEL SCRIPTS -->
+
+		<!-- data table - ajax -->
+		<script type="text/javascript" src="{{ url('public/backEnd/js/jquery.dataTables.min.js') }}"></script>
+		<script type="text/javascript" src="{{ url('public/backEnd/js/dataTables.tableTools.min.js') }}"></script>
+		<script type="text/javascript" src="{{ url('public/backEnd/js/dataTables.colReorder.min.js') }}"></script>
+		<script type="text/javascript" src="{{ url('public/backEnd/js/dataTables.scroller.min.js') }}"></script>
+		<script type="text/javascript" src="{{ url('public/backEnd/js/dataTables.bootstrap.js') }}"></script>
+		<script src="{{ url('public/backEnd/js/demo.js') }}" type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/datatable.js') }}"  type="text/javascript"></script>
+		<script src="{{ url(backEndJsPath.'/table-ajax.js') }}"  type="text/javascript"></script>
+        <!-- data-tables end -->
+
+		<script>
+		jQuery(document).ready(function() {    
+		   Metronic.init(); // init metronic core componets
+		   Layout.init(); // init layout
+		});
+		</script>
+        @yield('scripts')
+		<!-- END JAVASCRIPTS -->
+	</body>
+<!-- END BODY -->
+</html>
